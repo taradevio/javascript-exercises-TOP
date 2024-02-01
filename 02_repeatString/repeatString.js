176d1ab4676b77a67a8c7758bb8cab4a8dfcb4c8
Nanda Kresnatara <nandayavanets@mail.ru>
2024-02-01 13:26:30 +0000

    aolve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, num) {
+        let hey = "";
+        for (; num > 0; num--) {
+            hey += str;
+        }
+        return hey;
+    };
+    repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
